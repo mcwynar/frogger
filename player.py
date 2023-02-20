38f2279febc3358387633ccb52f380795803f27e
Michal <mcwynar04@gmail.com>
2023-02-20 18:32:55 +0100

    Player Movement

--- player.py
+++ player.py
@@ -1,22 +1,45 @@
 import pygame
 
+
 class Player(pygame.sprite.Sprite):
     def __init__(self, position, groups):
         super().__init__(groups)
-        self.image = pygame.Surface((50,50))
+        self.image = pygame.Surface((50, 50))
         self.image.fill('red')
         self.rect = self.image.get_rect(center=position)
 
+        # float based movement
+        self.pos = pygame.math.Vector2(self.rect.center)
+        self.direction = pygame.math.Vector2()
+        self.speed = 200
+
+    def move(self, dt):
+        # normalize a vector -> the length of a vector is going to be 1
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize()
+
+        self.pos += self.direction * self.speed * dt
+        self.rect.center = (round(self.pos.x), round(self.pos.y))
+
     def input(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_w] or keys[pygame.K_UP]:
-            print('up')
-        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
-            print('down')
-        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
-            print('left')
-        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
-            print('right')
 
-    def update(self):
+        # vertical input
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
+            self.direction.y = -1
+        elif keys[pygame.K_s] or keys[pygame.K_DOWN]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+
+        # horizontal input
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        elif keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        else:
+            self.direction.x = 0
+
+    def update(self, dt):
         self.input()
+        self.move(dt)
